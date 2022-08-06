08f81cd493caed463df34637d3c08d21cbf10979
sp-hill <spencer.hill@npgr.com>
2022-08-05 22:54:42 -0400

    Completed part 11 of the tutorial

--- actions.py
+++ actions.py
@@ -156,3 +156,18 @@ class BumpAction(ActionWithDirection):
 			return MeleeAction(self.entity, self.dx, self.dy).perform()
 		else:
 			return MovementAction(self.entity, self.dx, self.dy).perform()
+
+class TakeStairsAction(Action):
+	def perform(self) -> None:
+		"""
+		Take the stairs, if any exist at the entity's location.
+		"""
+		if(self.entity.x, self.entity.y) == self.engine.game_map.downstairs_location:
+			self.engine.game_world.generate_floor()
+			self.engine.message_log.add_message(
+				"You descend the staircase.",
+				color.descend
+			)
+		else:
+			raise exceptions.Impossible("There are no stairs here.")
+
